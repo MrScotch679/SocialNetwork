233a6dd8da8f4af589c53f70865da0f2493bf85d
Ulad <vladis67948@gmail.com>
2024-07-03 21:51:38 +0300

    feat(translation): added i18n and config

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 import { App } from './app/App.tsx'
 import { ThemeProvider } from './app/providers/theme-provider/index.ts'
 
+import './shared/config/translation/translation.ts'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<ThemeProvider>
