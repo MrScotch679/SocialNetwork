364b58f05614574b3dfa5231005c3b949e501c79
Ulad <vladis67948@gmail.com>
2024-07-06 19:12:52 +0300

    fix(layout): styles fix

--- src/widgets/layout/ui/layout.tsx
+++ src/widgets/layout/ui/layout.tsx
@@ -1,14 +1,17 @@
-import { memo } from 'react'
+import { memo, Suspense } from 'react'
 
 import styles from './layout.module.scss'
 import { Navbar } from '@/widgets/navbar'
 import { Outlet } from 'react-router-dom'
+import { PageLoader } from '@/widgets/page-loader'
 
 export const Layout = memo(() => {
 	return (
 		<div className={styles.layout}>
 			<Navbar />
-			<Outlet />
+			<Suspense fallback={<PageLoader />}>
+				<Outlet />
+			</Suspense>
 		</div>
 	)
 })
