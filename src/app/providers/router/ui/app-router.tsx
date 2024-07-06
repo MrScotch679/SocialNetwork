355949f9d26dfba9b593b6a28c3207e2c8267394
Ulad <vladis67948@gmail.com>
2024-07-06 19:13:25 +0300

    fix(app): global styles fix

--- src/app/providers/router/ui/app-router.tsx
+++ src/app/providers/router/ui/app-router.tsx
@@ -1,11 +1,7 @@
 import { routeConfig } from '@/shared/config/route-config/route-config'
-import { Suspense, memo } from 'react'
+import { memo } from 'react'
 import { RouterProvider } from 'react-router-dom'
 
 export const AppRouter = memo(() => {
-	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<RouterProvider router={routeConfig} />
-		</Suspense>
-	)
+	return <RouterProvider router={routeConfig} />
 })
