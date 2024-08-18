e27c4c300fc3da37e7606c45e895785e26dda7e5
Ulad <vladis67948@gmail.com>
2024-08-18 18:26:40 +0200

    task(navbar): added translation button

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -2,20 +2,21 @@ import { FC, memo } from 'react'
 import styles from './navbar.module.scss'
 import { ThemeSwitcher } from '@/widgets/theme-switcher'
 import { useTranslation } from 'react-i18next'
-import { Button } from '@/shared/ui/button'
+import { LanguageSelect } from '@/widgets/language-select'
 
 interface NavbarProps {}
 
 export const Navbar: FC<NavbarProps> = memo(() => {
-	const { t, i18n } = useTranslation()
+	const { t } = useTranslation()
 
 	return (
 		<div className={styles.navbar}>
-			<ThemeSwitcher />
-
-			<Button onClick={() => i18n.changeLanguage('ru')}>S</Button>
-
 			{t('Title')}
+
+			<div className={styles.platformSettings}>
+				<LanguageSelect />
+				<ThemeSwitcher />
+			</div>
 		</div>
 	)
 })
