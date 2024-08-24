07e43e0152fa8464e8c142dc4d008540e1d43ea0
Ulad <vladis67948@gmail.com>
2024-08-24 14:03:34 +0200

    feat(modal): added animation modal

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,10 +1,18 @@
-import { FC, memo, PropsWithChildren, useEffect } from 'react'
+import {
+	FC,
+	memo,
+	PropsWithChildren,
+	useCallback,
+	useEffect,
+	useState,
+} from 'react'
 
 import styles from './modal.module.scss'
 import { className } from '@/shared/lib/className/className'
 import { Button } from '@/shared/ui/button'
 import { ButtonMode } from '../button/button.types'
 import { createPortal } from 'react-dom'
+import { ModalState } from './modal.types'
 
 interface ModalProps extends PropsWithChildren {
 	isOpen: boolean
@@ -14,10 +22,20 @@ interface ModalProps extends PropsWithChildren {
 export const Modal: FC<ModalProps> = memo(props => {
 	const { isOpen, onClose, children } = props
 
+	if (!isOpen) {
+		return null
+	}
+
+	const [modalAnamation, setModalAnamation] = useState(ModalState.OPEN)
+
+	const handleClose = useCallback(() => {
+		setModalAnamation(ModalState.CLOSE)
+	}, [onClose])
+
 	useEffect(() => {
 		const handleEsc = (event: KeyboardEvent) => {
 			if (event.code === 'Escape') {
-				onClose()
+				handleClose()
 			}
 		}
 
@@ -33,17 +51,20 @@ export const Modal: FC<ModalProps> = memo(props => {
 			className={styles.modalOverlay}
 			onClick={event => {
 				if (event.target === event.currentTarget) {
-					onClose()
+					handleClose()
 				}
 			}}
 		>
 			<div
-				className={className(styles.modal, undefined, [
-					isOpen ? styles.open : styles.close,
-				])}
+				className={className(styles.modal, undefined, [styles[modalAnamation]])}
+				onAnimationEnd={event => {
+					if (event?.animationName?.includes('modalFadeOut')) {
+						onClose()
+					}
+				}}
 			>
 				<Button
-					onClick={onClose}
+					onClick={handleClose}
 					className={styles.closeButton}
 					mode={ButtonMode.PRIMARY}
 				>
@@ -57,6 +78,6 @@ export const Modal: FC<ModalProps> = memo(props => {
 
 	return createPortal(
 		modalComponent,
-		document?.getElementsByClassName('app')[0] as HTMLElement
+		document?.getElementsByClassName('app')?.[0] as HTMLElement
 	)
 })
---
+++ src/shared/ui/modal/modal.types.ts
@@ -0,0 +1,4 @@
+export enum ModalState {
+	OPEN = 'open',
+	CLOSE = 'close',
+}
