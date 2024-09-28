ce018893de6ebd1d6241e86873f51cfe33943c3f
Ulad <vladis67948@gmail.com>
2024-09-28 19:46:01 +0200

    fix(app_link): added styles fix

--- src/shared/ui/app-link/app-link.tsx
+++ src/shared/ui/app-link/app-link.tsx
@@ -1,12 +1,15 @@
 import { FC, PropsWithChildren, memo } from 'react'
 import styles from './app-link.module.scss'
 import { Link, LinkProps } from 'react-router-dom'
+import { classNames } from '@/shared/lib/classNames/classNames'
 
-interface AppLinkProps extends PropsWithChildren, LinkProps {}
+interface AppLinkProps extends PropsWithChildren, LinkProps {
+	className?: string
+}
 
-export const AppLink: FC<AppLinkProps> = memo(({ to, children }) => {
+export const AppLink: FC<AppLinkProps> = memo(({ to, children, className }) => {
 	return (
-		<Link to={to} className={styles.link}>
+		<Link to={to} className={classNames(styles.link, {}, [className])}>
 			{children}
 		</Link>
 	)
