aca636fda3cc58be0a1e318c4e8ce46458a8c9ca
Ulad <vladis67948@gmail.com>
2024-10-12 13:33:00 +0200

    feat(profile): added profile

--- src/entities/profile/ui/profile-card/profile-card.tsx
+++ src/entities/profile/ui/profile-card/profile-card.tsx
@@ -1,17 +1,115 @@
-import { memo } from 'react'
+import { ChangeEvent, FC, memo } from 'react'
 
-import { useAppSelector } from '@/shared/hooks/redux-hooks'
-import { getProfile } from '../../model/selectors/get-profile/get-profile'
+import { Profile } from '../../model/types/profile'
+import { Input } from '@/shared/ui/input/input'
+import { useTranslation } from 'react-i18next'
+import { Avatar } from '@/shared/ui/avatar'
+import { Currency } from '@/shared/types/currency'
+import { Сities } from '@/shared/types/cities'
+import { Select } from '@/shared/ui/select'
 
-import { getProfileLoading } from '../../model/selectors/get-profile-loading/get-profile-loading'
-import { getProfileError } from '../../model/selectors/get-profile-error/get-profile-error'
+export interface ProfileCardProps {
+	profileData?: Profile
+	isLoading?: boolean
+	errorProfile?: string
+	readonly?: boolean
+	onChangeFirstName?: (event: ChangeEvent<HTMLInputElement>) => void
+	onChangeLastName?: (event: ChangeEvent<HTMLInputElement>) => void
+	onChangeAge?: (event: ChangeEvent<HTMLInputElement>) => void
+	onChangeCountry?: (event: ChangeEvent<HTMLInputElement>) => void
+	onChangeCity?: (value: Сities) => void
+	onChangeCurrency?: (value: Currency) => void
+	onChangeUsername?: (event: ChangeEvent<HTMLInputElement>) => void
+	onChangeAvatar?: (event: ChangeEvent<HTMLInputElement>) => void
+}
 
-import styles from './profile-card.module.scss'
+export const ProfileCard: FC<ProfileCardProps> = memo(props => {
+	const {
+		profileData,
+		isLoading,
+		errorProfile,
+		readonly,
+		onChangeFirstName,
+		onChangeLastName,
+		onChangeAge,
+		onChangeCountry,
+		onChangeCity,
+		onChangeCurrency,
+		onChangeUsername,
+		onChangeAvatar,
+	} = props
 
-export const ProfileCard = memo(() => {
-	const profileData = useAppSelector(getProfile)
-	const isLoading = useAppSelector(getProfileLoading)
-	const errorProfile = useAppSelector(getProfileError)
+	const { t } = useTranslation()
 
-	return <div>{'ProfileCard'}</div>
+	return (
+		<div>
+			<Avatar src={profileData?.avatar} />
+
+			<Input
+				disabled={readonly}
+				label={t('First name')}
+				value={profileData?.firstName}
+				onChange={onChangeFirstName}
+			/>
+
+			<Input
+				disabled={readonly}
+				label={t('Last name')}
+				value={profileData?.lastName}
+				onChange={onChangeLastName}
+			/>
+
+			<Input
+				disabled={readonly}
+				label={t('Username')}
+				value={profileData?.username}
+				onChange={onChangeUsername}
+			/>
+
+			<Input
+				disabled={readonly}
+				label={t('Avatar')}
+				value={profileData?.avatar}
+				onChange={onChangeAvatar}
+			/>
+
+			<Input
+				disabled={readonly}
+				label={t('Age')}
+				value={profileData?.age}
+				onChange={onChangeAge}
+			/>
+
+			<Input
+				disabled={readonly}
+				label={t('Country')}
+				value={profileData?.country}
+				onChange={onChangeCountry}
+			/>
+
+			<Select
+				value={''}
+				label={t('City')}
+				options={[
+					{
+						value: Сities.MOSCOW,
+						label: t('City.MOSCOW'),
+					},
+				]}
+				onChange={onChangeCity}
+			/>
+
+			<Select
+				label={t('Currency')}
+				value={''}
+				options={[
+					{
+						value: Currency.USD,
+						label: t('Currency.USD'),
+					},
+				]}
+				onChange={onChangeCurrency}
+			/>
+		</div>
+	)
 })
--- src/pages/profile/ui/profile.tsx
+++ src/pages/profile/ui/profile.tsx
@@ -1,10 +1,84 @@
-import { getProfileData, ProfileCard, profileReducer } from '@/entities/profile'
-import { useAppDispatch } from '@/shared/hooks/redux-hooks'
+import {
+	getForm,
+	getProfileData,
+	getProfileError,
+	getProfileLoading,
+	getReadonly,
+	getValidateErrors,
+	profileActions,
+	ProfileCard,
+	profileReducer,
+} from '@/entities/profile'
+import { useAppDispatch, useAppSelector } from '@/shared/hooks/redux-hooks'
 import { ModuleLoader } from '@/shared/lib/module-loader'
-import { memo, useEffect } from 'react'
+import { ChangeEvent, memo, useCallback, useEffect } from 'react'
+import { Header } from './header'
+import { Currency } from '@/shared/types/currency'
+import { Сities } from '@/shared/types/cities'
 
 const Profile = memo(() => {
 	const dispatch = useAppDispatch()
+
+	const profileForm = useAppSelector(getForm)
+	const isLoading = useAppSelector(getProfileLoading)
+	const errorProfile = useAppSelector(getProfileError)
+	const readonly = useAppSelector(getReadonly)
+	const validateErrors = useAppSelector(getValidateErrors)
+
+	const onChangeFirstName = useCallback(
+		(event: ChangeEvent<HTMLInputElement>) => {
+			dispatch(profileActions.updateProfile({ firstName: event.target.value }))
+		},
+		[]
+	)
+
+	const onChangeLastName = useCallback(
+		(event: ChangeEvent<HTMLInputElement>) => {
+			dispatch(profileActions.updateProfile({ lastName: event.target.value }))
+		},
+		[]
+	)
+
+	const onChangeAge = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+		const regexNumberAndEmptyValue = /^[0-9]+$|$/
+
+		if (!regexNumberAndEmptyValue.test(event.target.value)) {
+			return
+		}
+
+		dispatch(
+			profileActions.updateProfile({ age: Number(event.target.value) || 0 })
+		)
+	}, [])
+
+	const onChangeCountry = useCallback(
+		(event: ChangeEvent<HTMLInputElement>) => {
+			dispatch(profileActions.updateProfile({ country: event.target.value }))
+		},
+		[]
+	)
+
+	const onChangeCity = useCallback((value: Сities) => {
+		dispatch(profileActions.updateProfile({ city: value }))
+	}, [])
+
+	const onChangeCurrency = useCallback((value: Currency) => {
+		dispatch(profileActions.updateProfile({ currency: value }))
+	}, [])
+
+	const onChangeUsername = useCallback(
+		(event: ChangeEvent<HTMLInputElement>) => {
+			dispatch(profileActions.updateProfile({ username: event.target.value }))
+		},
+		[]
+	)
+
+	const onChangeAvatar = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+		dispatch(profileActions.updateProfile({ avatar: event.target.value }))
+	}, [])
+
+	// 	username
+	// avatar
 
 	useEffect(() => {
 		dispatch(getProfileData())
@@ -12,7 +86,22 @@ const Profile = memo(() => {
 
 	return (
 		<ModuleLoader reducerKey='profile' reducer={profileReducer}>
-			<ProfileCard />
+			<Header />
+
+			<ProfileCard
+				readonly={readonly}
+				profileData={profileForm}
+				isLoading={isLoading}
+				errorProfile={errorProfile}
+				onChangeFirstName={onChangeFirstName}
+				onChangeLastName={onChangeLastName}
+				onChangeAge={onChangeAge}
+				onChangeCity={onChangeCity}
+				onChangeCountry={onChangeCountry}
+				onChangeCurrency={onChangeCurrency}
+				onChangeUsername={onChangeUsername}
+				onChangeAvatar={onChangeAvatar}
+			/>
 		</ModuleLoader>
 	)
 })
