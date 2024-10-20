3b6caccf9386252d57a1e7939de5b2d2162d3d01
Ulad <vladis67948@gmail.com>
2024-10-20 17:22:11 +0200

    feat(article_comments): added index

--- src/pages/article-details/index.ts
+++ src/pages/article-details/index.ts
@@ -1,1 +1,7 @@
 export { ArticleDetails } from './ui/article-details'
+
+export type { ArticlesCommentsSchema } from './model/types/articles-comments-schema'
+export {
+	articlesCommentsActions,
+	articlesCommentsReducer,
+} from './model/slices/articles-comments-slice'
