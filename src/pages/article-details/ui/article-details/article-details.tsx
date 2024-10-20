3746fd1ae902c1ff91d447021b08b4401f30b86f
Ulad <vladis67948@gmail.com>
2024-10-20 17:20:51 +0200

    feat(article_comments): added ui

--- src/pages/article-details/ui/article-details/article-details.tsx
+++ src/pages/article-details/ui/article-details/article-details.tsx
@@ -1,11 +1,47 @@
 import { Article } from '@/entities/article'
-import { memo } from 'react'
+import { CommentsList } from '@/entities/comment'
+import { ModuleLoader } from '@/shared/lib/module-loader'
+import { memo, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import {
+	articlesCommentsReducer,
+	getArticleComments,
+} from '../../model/slices/articles-comments-slice'
+import { useAppDispatch, useAppSelector } from '@/shared/hooks/redux-hooks'
+import { getArticleCommentsLoading } from '../../model/selectors/get-article-comments-loading/get-article-comments-loading'
+import { getArticleCommentsError } from '../../model/selectors/get-article-comments-error/get-article-comments-error'
+import { getCommentsByActicleId } from '../../model/services/get-comments-by-acticle-id/get-comments-by-acticle-id'
 
 const ArticleDetails = memo(() => {
 	const { id } = useParams()
 
-	return <>{id ? <Article articleId={id} /> : 'Not found'}</>
+	const dispatch = useAppDispatch()
+
+	const comments = useAppSelector(getArticleComments.selectAll)
+	const commentsIsLoading = useAppSelector(getArticleCommentsLoading)
+	const commentError = useAppSelector(getArticleCommentsError)
+
+	useEffect(() => {
+		if (id) {
+			dispatch(getCommentsByActicleId(id))
+		}
+	}, [id])
+
+	return (
+		<ModuleLoader
+			reducerKey='articleComments'
+			reducer={articlesCommentsReducer}
+		>
+			<div>
+				{id ? <Article articleId={id} /> : 'Not found'}
+				<CommentsList
+					isLoading={commentsIsLoading}
+					error={commentError}
+					comments={comments}
+				/>
+			</div>
+		</ModuleLoader>
+	)
 })
 
 export default ArticleDetails
